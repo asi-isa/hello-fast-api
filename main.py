fc0af5cf8c44514b5d552d0621c105872b2726c1
asi-isa <ali.inceoglu@protonmail.com>
2026-09-10 14:40:20 +0200

    query paramater

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ async def root():
 
 
 @app.get("/items/{item_id}")
-async def get_item(item_id: int):
-    return {"item": item_id}
+async def get_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
