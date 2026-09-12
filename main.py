d6c986440cf199d2109eb5a62ae6569774590fe5
asi-isa <ali.inceoglu@protonmail.com>
2026-09-12 12:47:35 +0200

    request body

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 app = FastAPI()
 
@@ -12,3 +21,19 @@ async def root():
 async def get_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    print("received data", item)
+    print("dump", item.model_dump())
+    item.name = "Pong"
+    return item
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
